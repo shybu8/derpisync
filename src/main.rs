ac924c9d4ecc49bb95519a97d73e2abec59965f6
shybu8 <bushink04@proton.me>
2025-09-15 23:17:14 +0300

    Basic tags fetch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct ImagesEndpoint {
+    pub image: Image,
+}
+
+#[derive(Debug, Deserialize)]
+struct Image {
+    pub tags: Vec<String>,
+}
+
 fn main() {
+    let text = reqwest::blocking::get("https://derpibooru.org/api/v1/json/images/1")
+        .expect("Response receiveal error")
+        .text()
+        .expect("Getting responce text error");
+    let img = serde_json::from_str::<ImagesEndpoint>(&text);
+    dbg!(&img);
     println!("Hello, world!");
 }
